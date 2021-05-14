6195ec2e477a66ad268ac962c74f57309c735aaa
Olivier-BeCode <76862461+Olivier-BeCode@users.noreply.github.com>
2021-05-14 17:38:55 +0200

    Fail

--- app/app.js
+++ app/app.js
@@ -1,11 +1,27 @@
 
 const socket = io('ws://localhost:8080');
 
+var messages = ["1 ", "2 ", "3 ", "4 ", "5 ", "6 "]
+
+ul = document.getElementById("messages")
+messages.forEach(function (message) {
+    let li = document.createElement('li');
+    ul.appendChild(li);
+
+    li.innerHTML += message;
+});
+
 socket.on('message', text => {
 
-    const el = document.createElement('li');
-    el.innerText = text;
-    document.querySelector('ul').appendChild(el)
+    messages.push(text)
+    messages.shift()
+    
+    ul = document.getElementById("messages")
+
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    
+    li.innerHTML = messages[messages.length - 1];
 
 });
 
@@ -15,3 +31,4 @@ document.querySelector('button').onclick = () => {
     socket.emit('message', text)
     document.querySelector('input').value='';
 }
+
